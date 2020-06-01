b56324461271239f789536d5a095ca107bd6d19b
Marc <marc13000@Jaidityas-MacBook-Air.local>
2020-06-01 22:20:46 +0530

    Update ventilaterService.js

--- src/components/calculator/ventilaterService.js
+++ src/components/calculator/ventilaterService.js
@@ -1,7 +1,32 @@
 const calculateValueFromOperation = (outputProp, inputParameters) => {
   console.log("checkk");
-
-  return "temp";
+  var out;
+  var oper = outputProp["operation"];
+oper[0]=NumberConvert(oper[0], inputParameters);
+oper[2]=NumberConvert(oper[2], inputParameters);
+    switch(oper[1]){
+        case "*":out=oper[0]*oper[2];
+                break;
+        case "/":out=oper[0]/oper[2];
+                break;
+        case "+":out=oper[0]+oper[2];
+                break;
+        case "-":out=oper[0]-oper[2];
+                break;
+    }
+let result = out;
+return result;
 };
 
 export { calculateValueFromOperation };
+const NumberConvert=(para, ip)=>
+{
+    var c=para.charAt(0);
+    if(typeof(c)==Number) {
+        para=Number(para);
+    }
+    else {
+        para="Null";
+    }
+    return para;
+}
