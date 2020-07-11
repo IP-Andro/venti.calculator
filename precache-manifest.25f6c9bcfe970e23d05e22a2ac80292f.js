3e79ec75370e4177d77a572808c4fe1b6ffff23c
Singh, Indra Preet <indrapreetsingh@gmail.com>
2020-07-11 11:40:45 +0530

    Updates

---
+++ precache-manifest.25f6c9bcfe970e23d05e22a2ac80292f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bfef9697946c9a3070ed66d1152a101",
+    "url": "/venti.calculator/index.html"
+  },
+  {
+    "revision": "7834dfde12247da49150",
+    "url": "/venti.calculator/static/css/main.a9dc8153.chunk.css"
+  },
+  {
+    "revision": "1636b33cf57878d163c7",
+    "url": "/venti.calculator/static/js/2.551e9f06.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/venti.calculator/static/js/2.551e9f06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7834dfde12247da49150",
+    "url": "/venti.calculator/static/js/main.9a2ab472.chunk.js"
+  },
+  {
+    "revision": "17233a023abc8696465c",
+    "url": "/venti.calculator/static/js/runtime-main.d387df53.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ef51142ebf0943a0b7f8396a8195dc05.js"
+  "/venti.calculator/precache-manifest.25f6c9bcfe970e23d05e22a2ac80292f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/venti.calculator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
