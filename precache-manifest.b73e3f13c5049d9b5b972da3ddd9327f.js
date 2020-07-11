14949e67725d679fe20a604202722f757d4dba34
Singh, Indra Preet <indrapreet.singh@barco.com>
2020-07-11 14:47:18 +0530

    waheguru buil

---
+++ precache-manifest.b73e3f13c5049d9b5b972da3ddd9327f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7daef2d11b8472d991964f68320df07f",
+    "url": "/venti.calculator/index.html"
+  },
+  {
+    "revision": "37a751240005b6fbd6e2",
+    "url": "/venti.calculator/static/css/main.13144b3d.chunk.css"
+  },
+  {
+    "revision": "0ecaf017d390cd0509aa",
+    "url": "/venti.calculator/static/js/2.cb6d3718.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/venti.calculator/static/js/2.cb6d3718.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37a751240005b6fbd6e2",
+    "url": "/venti.calculator/static/js/main.e5e9b583.chunk.js"
+  },
+  {
+    "revision": "5b54bb953565e737b309",
+    "url": "/venti.calculator/static/js/runtime-main.09ccd3e8.js"
+  }
+]);
